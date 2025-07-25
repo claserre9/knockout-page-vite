5629d2c2a72c22cd6efb751566116b807a344044
Clifford Laserre <clifford.l@valnetinc.com>
2025-07-25 13:34:55 -0400

    Remove unused "Home" navigation link from `AppViewModel`.

--- src/components/AppViewModel.ts
+++ src/components/AppViewModel.ts
@@ -9,7 +9,6 @@ export class AppViewModel extends BaseViewModel{
                 <h1>App</h1>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
                     </ul>
                 </nav>
